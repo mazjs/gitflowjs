0e6619c59a7d5d8bb94d33293a8de2ecdbe0f9fe
unknown <Mazharul@MIS-11.jobst.jobstreet.com>
2015-03-24 15:41:44 +0800

    Adding more function in core.

--- Db.php
+++ Db.php
@@ -4,5 +4,11 @@ class Db {
 
 	function __construct(){
 		//nmothing to do for now really
+		//hmm, should have something to do with this
+	}
+
+	function query(){
+		//query me ?
+		// maybe not!
 	}
 }
